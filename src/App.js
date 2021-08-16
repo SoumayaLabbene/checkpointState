af686bf4ff6333753c434c5a03f4a92c18953337
SoumayaLabbene <soumaya.labbene@medtech.tn>
2021-08-16 12:04:14 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    fullName: "Soumaya Labbene",
+    bio: "22 years old",
+    imgSrc: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png",
+    profession: "Software Engineer",
+    date: new Date(),
+    shows: false
+  };
+  handleClick = () => this.setState({ shows: !this.state.shows })
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  componentDidUpdate() {
+    clearInterval(this.timerID);
+    alert(this.state.date)
+  }
+  render(){
+    return(
+     
+      <div style={{ textAlign: 'center', marginTop: '60px' }}>
+         <h2>{this.state.date.toLocaleTimeString()}</h2>
+          <button onClick={this.handleClick} style={{backgroundColor:'#A34B73', color:'white', borderColor:'#A34B73'}}> ClickMe</button>
+          
+          {
+            (this.state.shows) ? (
+        <>
+              <h1>{this.state.fullName}</h1>
+                <h3>{this.state.bio},</h3>
+                <h3>{this.state.profession}</h3>
+                <br></br>
+            <img src={this.state.imgSrc} alt="profile" height="500px" width="500px"></img>
+          
+        </>
+            ) : (
+                <div style={{color:'#D483A3'}}>
+                  You have to click on the button !
+                </div>
+          )
+          }
+      
+      </div>
+    
+    )  }
+ }
 
 export default App;
